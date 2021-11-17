a1f7ad64e72b3e53957d0c13d999a29f4ea75686
igoreta <ivoronline@gmail.com>
2021-11-17 07:05:01 +0100

    Commit

--- src/main/java/com/ivoronline/springboot_json_deserializer_alternative/DTO/PersonDTO.java
+++ src/main/java/com/ivoronline/springboot_json_deserializer_alternative/DTO/PersonDTO.java
@@ -4,18 +4,18 @@ public class PersonDTO {
 
   //PROPERTIES
   public String propName;
-  public Float  propHeight;                      //TO STORE FORMATTED FLOAT VALUE 1.67
+  public Float  propHeight;                      //To store formatted float value 1.67
 
-  //CONSTRUCTOR                                  //DESERIALIZATION USES CONSTRUCTOR IF AVAILABLE
-  PersonDTO(String name, String height) {        //STRING HEIGHT TO STORE COMMA SEPARATED JSON STRING "1,67"
-    this.propName = name;                        // PROPERTY THAT DOESN'T NEED CUSTOM REFORMATTING
-    convertHeight(height);                       //PROPERTY THAT NEEDS CUSTOM DESERIALIZATION
+  //CONSTRUCTOR                                  //Deserialization uses constructor if available
+  PersonDTO(String name, String height) {        //String height to store comma separated json string "1,67"
+    this.propName = name;                        //Property that doesn't need reformatting
+    convertHeight(height);                       //Property that needs        reformatting
   }
 
   //CONVERTERS
   public void convertHeight(String height) {
-    String heightFormatted = height.replace(",", ".");           //HEIGHT FROM JSON: REPLACE DOT WITH COMMA
-    this.propHeight        = Float.parseFloat(heightFormatted);  //HEIGHT FROM DTO:  PARSE STRING INTO FLOAT
+    String heightFormatted = height.replace(",", ".");           //Height from json: replace dot with comma
+    this.propHeight        = Float.parseFloat(heightFormatted);  //Height from dto:  parse string into float
   }
 
 }
