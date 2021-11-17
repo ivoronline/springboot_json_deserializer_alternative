cd87553c80ccc1fbfd1a8bb52e4fc0cbb6894e1d
igoreta <ivoronline@gmail.com>
2021-11-17 07:02:40 +0100

    Commit

--- src/main/java/com/ivoronline/springboot_json_deserializer_alternative/DTO/PersonDTO.java
+++ src/main/java/com/ivoronline/springboot_json_deserializer_alternative/DTO/PersonDTO.java
@@ -8,14 +8,14 @@ public class PersonDTO {
 
   //CONSTRUCTOR                                  //DESERIALIZATION USES CONSTRUCTOR IF AVAILABLE
   PersonDTO(String name, String height) {        //STRING HEIGHT TO STORE COMMA SEPARATED JSON STRING "1,67"
-    this.propName = name;                        //DESERIALIZED PROPERTY THAT DOESN'T NEED CUSTOM REFORMATTING
+    this.propName = name;                        // PROPERTY THAT DOESN'T NEED CUSTOM REFORMATTING
     convertHeight(height);                       //PROPERTY THAT NEEDS CUSTOM DESERIALIZATION
   }
 
   //CONVERTERS
   public void convertHeight(String height) {
-    height          = height.replace(",", ".");  //HEIGHT FROM JSON: REPLACE DOT WITH COMMA
-    this.propHeight = Float.parseFloat(height);  //HEIGHT FROM DTO:  PARSE STRING INTO FLOAT
+    String heightFormatted = height.replace(",", ".");           //HEIGHT FROM JSON: REPLACE DOT WITH COMMA
+    this.propHeight        = Float.parseFloat(heightFormatted);  //HEIGHT FROM DTO:  PARSE STRING INTO FLOAT
   }
 
 }
